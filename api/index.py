d38fb9a451b3deefe497f5eb884be64c73f4bc73
Muhammad Azeem <muhammad.azeem@cybergen.com>
2025-08-28 16:46:45 +0500

    update

--- api/index.py
+++ api/index.py
@@ -801,5 +801,5 @@ def get_top_delay_reasons():
     }
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
